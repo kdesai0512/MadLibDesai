cc53e269c842f6532eba9d59034bbf2792c97dbe
kdesai0512 <desai8252@students.d211.org>
2019-09-05 00:23:50 -0500

    Finished except for dimensions and a little formatting. End of Wed.

--- app/src/main/java/com/example/madlibs/CompleteActivity.java
+++ app/src/main/java/com/example/madlibs/CompleteActivity.java
@@ -10,10 +10,15 @@ import android.widget.TextView;
 
 public class CompleteActivity extends AppCompatActivity {
 
+
+    public static final String MY_NAME = "name";
     public static final String MY_ADJ = "adjective";
     public static final String MY_NOUN= "noun";
     public static final String MY_VERB = "verb";
     public static final String MY_NUM = "number";
+    public static final String MY_PLACE = "place";
+    public static final String MY_ANIMAL= "animal";
+    public static final String MY_VERB2 = "verb2";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,13 +26,20 @@ public class CompleteActivity extends AppCompatActivity {
         setContentView(R.layout.activity_complete);
 
         Intent intent = getIntent();
+        String myName = intent.getStringExtra(MY_NAME);
         String myAdj = intent.getStringExtra(MY_ADJ);
         String myNoun = intent.getStringExtra(MY_NOUN);
         String myVerb = intent.getStringExtra(MY_VERB);
         String myNum = intent.getStringExtra(MY_NUM);
+        String myPlace = intent.getStringExtra(MY_PLACE);
+        String myAnimal = intent.getStringExtra(MY_ANIMAL);
+        String myVerb2 = intent.getStringExtra(MY_VERB2);
 
-        String strToDisplay = "I love " + myAdj + " " + myNoun +
-                " because it " + myVerb + " for " + myNum + " hours.";
+
+
+        String strToDisplay = "One day a " + myAdj + " " + myNoun + " named " + myName + " built a time machine. " + myName + " decided to go to the year " +
+        myNum + " at the best place, " + myPlace + ". When " + myName + " got to " + myPlace + ", a " + myAnimal + " " + myVerb + " and " + myName
+                + " became more " + myAdj + ". " + myName + " " + myVerb2 + " and decided that going back in time was scary.";
         TextView str = (TextView) findViewById(R.id.info);
         str.setText(strToDisplay);
 
--- app/src/main/java/com/example/madlibs/MainActivity.java
+++ app/src/main/java/com/example/madlibs/MainActivity.java
@@ -18,12 +18,17 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void fillInTheBlanks(View v) {
+        EditText myName = (EditText) findViewById(R.id.name);
         EditText myAdj = (EditText) findViewById(R.id.adjective);
         EditText myNoun = (EditText) findViewById(R.id.noun);
         EditText myVerb = (EditText) findViewById(R.id.verb);
         EditText myNum = (EditText) findViewById(R.id.number);
+        EditText myPlace = (EditText) findViewById(R.id.place);
+        EditText myAnimal = (EditText) findViewById(R.id.animal);
+        EditText myVerb2 = (EditText) findViewById(R.id.verb2);
 
-        if (myAdj.length() == 0) {
+        if (myName.length() == 0 || myAdj.length() == 0 || myNoun.length() == 0 || myVerb.length() == 0 || myNum.length() == 0
+        || myPlace.length() == 0 || myAnimal.length() == 0 || myVerb2.length() == 0) {
             Context context = getApplicationContext();
             CharSequence text = "Please fill all blanks";
             int duration = Toast.LENGTH_SHORT;
@@ -32,20 +37,30 @@ public class MainActivity extends AppCompatActivity {
             toast.show();
         }
         else {
+            String myNameStr = myName.getText().toString();
             String myAdjStr = myAdj.getText().toString();
             String myNounStr = myNoun.getText().toString();
             String myVerbStr = myVerb.getText().toString();
             String myNumStr = myNum.getText().toString();
+            String myPlaceStr = myPlace.getText().toString();
+            String myAnimalStr = myAnimal.getText().toString();
+            String myVerb2Str = myVerb2.getText().toString();
 
 
             Intent intent = new Intent(this, CompleteActivity.class);
 
+            intent.putExtra(CompleteActivity.MY_NAME, myNameStr);
             intent.putExtra(CompleteActivity.MY_ADJ, myAdjStr);
             intent.putExtra(CompleteActivity.MY_NOUN, myNounStr);
             intent.putExtra(CompleteActivity.MY_VERB, myVerbStr);
             intent.putExtra(CompleteActivity.MY_NUM, myNumStr);
+            intent.putExtra(CompleteActivity.MY_PLACE, myPlaceStr);
+            intent.putExtra(CompleteActivity.MY_ANIMAL, myAnimalStr);
+            intent.putExtra(CompleteActivity.MY_VERB2, myVerb2Str);
+
 
             startActivity(intent);
         }
     }
+
 }
